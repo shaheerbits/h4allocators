e8c199ed7220d8e7ebeabbd810011c428833f81d
shaheerbits <shaheercodes.allday@gmail.com>
2026-06-20 14:28:02 +0530

    fix(auth): change doc comment to top level comment for file metadata

--- src/allocators/arena.zig
+++ src/allocators/arena.zig
@@ -1,11 +1,14 @@
-///
-/// Arena Allocator
-/// copyright © github.com/shaheerbits
-///
+//!
+//! Arena Allocator
+//! copyright © github.com/shaheerbits
+//!
+
 const std = @import("std");
 
 pub const ArenaAllocator = struct {
+    /// The buffer that defines a memory space
     buffer: []u8,
+    /// The bookmark that defines the offset (used memory)
     offset: usize,
 
     /// Initializes an ArenaAllocator. Returns a reference to the allocator.
@@ -18,13 +21,16 @@ pub const ArenaAllocator = struct {
 
     /// Allocates and return memory of a specific size.
     pub fn alloc(self: *ArenaAllocator, size: usize) ![]u8 {
+        // If memory space of size isn't available, return OutOfMemory (error)
         if (size > self.buffer.len - self.offset) {
             return error.OutOfMemory;
         }
 
+        // Storing current start offset, Changing the offset
         const start = self.offset;
         self.offset += size;
 
+        // Returning the memory (from start to current offset)
         return self.buffer[start..self.offset];
     }
 
@@ -51,18 +57,24 @@ pub const ArenaAllocator = struct {
 
     /// Aligns the memory offset by creating padding into the memory.
     fn alignOffset(self: *ArenaAllocator, alignment: usize) !void {
+        // Creates a padding using
         const padding = (alignment - (self.offset % alignment)) % alignment;
 
+        // Checks if adding the padding cause memory overflow
         if (padding > self.buffer.len - self.offset) {
             return error.OutOfMemory;
         }
 
+        // Adding the padding to the memory
         self.offset += padding;
     }
 
     /// Returns a pointer to undefined memory.
     pub fn create(self: *ArenaAllocator, comptime T: type) !*T {
         const memory = try self.allocAligned(@sizeOf(T), @alignOf(T));
+        // Casting a *u8 to *T using @ptrCast()
+        // The alignment may be different, therefore @alignCast() is used
+        // @alignCast assumes that the ptr is aligned
         return @ptrCast(@alignCast(memory.ptr));
     }
 
@@ -106,3 +118,37 @@ pub const ArenaAllocator = struct {
         return objects;
     }
 };
+
+test "alloc decreases the remaining size of memory" {
+    var buffer: [256]u8 = undefined;
+    var arena = ArenaAllocator.init(&buffer);
+
+    _ = try arena.alloc(100);
+
+    try std.testing.expectEqual(arena.remaining(), 156);
+}
+
+test "alloc increases the used size of memory" {
+    var buffer: [256]u8 = undefined;
+    var arena = ArenaAllocator.init(&buffer);
+
+    _ = try arena.alloc(100);
+    try std.testing.expectEqual(arena.used(), 100);
+
+    _ = try arena.alloc(20);
+    try std.testing.expectEqual(arena.used(), 120);
+
+    _ = try arena.alloc(16);
+    try std.testing.expectEqual(arena.used(), 136);
+}
+
+test "create returns a pointer to the type" {
+    const Player = struct { health: u32, x: f32, y: f32 };
+
+    var buffer: [256]u8 = undefined;
+    var arena = ArenaAllocator.init(&buffer);
+
+    const player = try arena.create(Player);
+
+    try std.testing.expectEqual(@TypeOf(player), *Player);
+}
--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,8 @@
-///
-/// h4allocators
-/// copyright © github.com/shaheerbits
-///
+//!
+//! h4allocators
+//! copyright © github.com/shaheerbits
+//!
+
 const std = @import("std");
 const allocators = @import("allocators/index.zig");
 // const arena_allocator = allocators.arena_allocator;
